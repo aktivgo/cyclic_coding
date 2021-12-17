e83385ecd655d072a77f36f2a93f7f920baf7d7f
aktivgo <AktivGo01062001@gmail.com>
2021-12-17 13:14:33 +0300

    made divide

---
+++ cyclic_coding/CyclicCoding.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cyclic_coding
+{
+    public static class CyclicCoding
+    {
+        public static KeyValuePair<List<List<int>>, List<int>> DividePolynomials(List<int> divisible, List<int> divider)
+        {
+            Stringify(divisible);
+            Stringify(divider);
+            
+            if (IsPolynomialLess(divisible, divider))
+            {
+                throw new ArgumentException("Делитель должен быть меньше делимого");
+            }
+
+            List<int> result = new List<int>();
+            List<List<int>> tailings = new List<List<int>>();
+            List<int> newDivisible = new List<int>(divisible);
+
+            // 1000/1010
+            while (!IsPolynomialLess(divider, newDivisible))
+            {
+                int k = newDivisible[0] / divider[0];
+                result.Add(k);
+                for (int i = divider.Count - 1; i >= 0; i--)
+                {
+                    newDivisible[i] += (k * divider[i]) % 2;
+                }
+                Stringify(newDivisible);
+                tailings.Add(newDivisible);
+            }
+            
+            return new KeyValuePair<List<List<int>>, List<int>>(tailings, result);
+        }
+
+        private static bool IsPolynomialLess(List<int> p1, List<int> p2)
+        {
+            if (p1.Count < p2.Count)
+            {
+                return true;
+            }
+            
+            // 1000 1011
+            if (p1.Count == p2.Count)
+            {
+                for (int i = 0; i < p1.Count; i++)
+                {
+                    if (p1[i] < p2[i])
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public static void Stringify(List<int> p)
+        {
+            while (p[0] == 0)
+            {
+                p.RemoveAt(0);
+            }
+        }
+    }
+}
--- cyclic_coding/Program.cs
+++ cyclic_coding/Program.cs
@@ -1,9 +1,34 @@
-﻿namespace cyclic_coding
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace cyclic_coding
 {
     internal static class Program
     {
         public static void Main(string[] args)
         {
+            List<int> p1 = new List<int>();
+            string input = Console.ReadLine();
+            Debug.Assert(input != null, nameof(input) + " != null");
+            foreach (var item in input)
+            {
+                p1.Add(int.Parse(item.ToString()));
+            }
+            
+            List<int> p2 = new List<int>();
+            input = Console.ReadLine();
+            Debug.Assert(input != null, nameof(input) + " != null");
+            foreach (var item in input)
+            {
+                p2.Add(int.Parse(item.ToString()));
+            }
+            KeyValuePair<List<List<int>>, List<int>> result = CyclicCoding.DividePolynomials(p1, p2);
+
+            foreach (var item in result.Value)
+            {
+                Console.Write(item);
+            }
         }
     }
 }
